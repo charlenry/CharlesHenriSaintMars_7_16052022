0a08bb4ee7774375af71bc5ee9f001b45401da8a
Charles-Henri St-Mars <ch.stmars@gmail.com>
2022-06-25 20:39:53 +0200

    Amélioration de l'interface

--- assets/js/templates/RecipeCard.js
+++ assets/js/templates/RecipeCard.js
@@ -35,7 +35,7 @@ class RecipeCard {
       <div class="recipe-photo"></div>
       <div class="recipe-name">${this._recipe.name}</div>
       <div class="recipe-time"><b><span><i class="far fa-regular fa-clock"></i></span>${this._recipe.time}</b></div>
-      <div class="recipe-ingredients">${this._recipe.ingredientsForOneRecipe}</div>
+      <div class="recipe-ingredients"><ul>${this._recipe.ingredientsForOneRecipe}</ul></div>
       <div class="recipe-description">${this._recipe.description}</div>
     `;
 
